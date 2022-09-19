5d66ad42687b6102ff36ed754beb6f48acf297ed
Jacobbidmead <jacob.bidmead.fullstack@outlook.com>
2022-09-19 16:13:01 +0700

    copied meta data to layout pages

---
+++ controllers/index.js
@@ -0,0 +1,23 @@
+router.get('/', (req, res) => {
+  // code here
+})
+
+router.get('./auth', (req, res) => {
+  // code here
+})
+
+router.get('./booking', (req, res) => {
+  // code here
+})
+
+router.get('./house', (req, res) => {
+  // code here
+})
+
+router.get('./profile', (req, res) => {
+  // code here
+})
+
+router.get('./reviews', (req, res) => {
+  // code here
+})
