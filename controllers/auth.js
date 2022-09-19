7fe72ec69f2a3c9beec946686a81ffb39ea2e921
Jacobbidmead <jacob.bidmead.fullstack@outlook.com>
2022-09-19 17:09:57 +0700

    added routers to controllers

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,24 @@
 const express = require('express')
 const router = express.Router()
 
+router.get('/login', (req, res) => {
+  res.render('../views')
+})
+
+router.get('/signup', (req, res) => {
+  res.render('../views')
+})
+
+router.post('/login', (req, res) => {
+  res.render('../views')
+})
+
+router.post('/login', (req, res) => {
+  res.render('../views')
+})
+
+router.get('/logout', (req, res) => {
+  res.render('../views')
+})
+
 module.exports = router
--- controllers/houses.js
+++ controllers/houses.js
@@ -1,4 +1,32 @@
 const express = require('express')
 const router = express.Router()
 
+router.get('/', (req, res) => {
+  res.render('../views')
+})
+
+router.get('/create', (req, res) => {
+  res.render('../views')
+})
+
+router.get('/:id', (req, res) => {
+  res.render('../views')
+})
+
+router.get('/:id/edit', (req, res) => {
+  res.render('../views')
+})
+
+router.post('/', (req, res) => {
+  res.render('../views')
+})
+
+router.patch('/:id', (req, res) => {
+  res.render('../views')
+})
+
+router.delete('/:id', (req, res) => {
+  res.render('../views')
+})
+
 module.exports = router
